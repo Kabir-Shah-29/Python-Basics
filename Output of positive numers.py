ddcf3353199af7442a00c20ab149b701eb808d35
Kabir-Shah-29 <68286737+Kabir-Shah-29@users.noreply.github.com>
2024-06-11 09:13:24 +0530

    Add files via upload

---
+++ Output of positive numers.py
@@ -0,0 +1,6 @@
+Python 3.9.0 (tags/v3.9.0:9cf6752, Oct  5 2020, 15:23:07) [MSC v.1927 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+===== RESTART: C:/Users/kabir/Desktop/Kabir/MY CAPTAIN/Positive numbers.py =====
+12 5 64 
+>>> 
